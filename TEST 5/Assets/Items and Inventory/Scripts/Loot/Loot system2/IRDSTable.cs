9caab1789adc801e8e871491d39ee1c8e857a164
Thomas Gaist <thomasgaist0@gmail.com>
2019-12-21 13:58:12 +0100

    Add Random Distribution System
    Start of RDS, more advanced loot drop system

---
+++ TEST 5/Assets/Items and Inventory/Scripts/Loot/Loot system2/IRDSTable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRDSTable: IRDSObject
+{
+	#region variables
+
+    int rdsCount { get; set; }
+
+    IEnumerable<IRDSObject> rdsContents { get; }
+    IEnumerable<IRDSObject> rdsResult { get; }
+
+
+	#endregion
+}
