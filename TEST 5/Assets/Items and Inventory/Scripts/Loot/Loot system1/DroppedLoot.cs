df0a5faf5c38386f61d222ce11382ae6e3218414
Thomas Gaist <thomasgaist0@gmail.com>
2020-01-19 14:48:54 +0100

    Show Loot name
    -add text object to DroppedLoot

--- TEST 5/Assets/Items and Inventory/Scripts/Loot/Loot system1/DroppedLoot.cs
+++ TEST 5/Assets/Items and Inventory/Scripts/Loot/Loot system1/DroppedLoot.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class DroppedLoot: MonoBehaviour
 {
     [SerializeField]
     private Item droppedLoot;
+
+    public Text lootNameText;
     
    
     public Item MyDroppedLoot { get => droppedLoot; set => droppedLoot = value; }
@@ -35,7 +38,9 @@ public class DroppedLoot: MonoBehaviour
 
     [SerializeField]
     private bool isInRange;
-  
+
+    //TIMING WHEN LOOT IS ON FLOOR
+    bool onFloorTimer = false;
 
     private void Awake()
     {
@@ -60,9 +65,13 @@ public class DroppedLoot: MonoBehaviour
         dropped = false;
 
         Physics2D.IgnoreLayerCollision(13, 13);
+        lootNameText = GetComponentInChildren<Text>();
+        lootNameText.text = MyDroppedLoot.ItemName;
+        lootNameText.enabled = false;
+        lootNameText.GetComponentInParent<Image>().enabled = false;
         //Physics2D.IgnoreLayerCollision(13, 11);
-        
-            
+
+        StartCoroutine(DropTimer());
     }
     private void Update()
     {  
@@ -113,5 +122,42 @@ public class DroppedLoot: MonoBehaviour
           
         }
 
+    }
+    private void OnMouseDown()
+    {
+        if (!inventory.IsFull())
+        {
+            inventory.AddItem(Instantiate(MyDroppedLoot));
+            Destroy(gameObject);
+        }
+    }
+    private void OnMouseOver()
+    {
+        ShowLootName();
+        
+    }
+    private void OnMouseExit()
+    {
+        HideLootName();
+    }
+
+    public void ShowLootName()
+    {
+        lootNameText.enabled = true;
+        lootNameText.GetComponentInParent<Image>().enabled = true;
+    }
+    public void HideLootName()
+    {
+        lootNameText.enabled = false;
+        lootNameText.GetComponentInParent<Image>().enabled = false;
+    }
+
+    //COROUTINE FOR TIMING DELAY BEFORE TOOLTIP AND PICKUP
+
+    IEnumerator DropTimer()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        onFloorTimer = true;
+        StopCoroutine(DropTimer());
     }
 }
