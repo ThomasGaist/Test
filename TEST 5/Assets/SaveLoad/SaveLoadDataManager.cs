ef271c831e8a55a4e55e85923078a3e7163d7ff5
Thomas Gaist <thomasgaist0@gmail.com>
2020-01-22 21:31:06 +0100

    Easy Save implementation
    -add saveloaddatamanager script

---
+++ TEST 5/Assets/SaveLoad/SaveLoadDataManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadDataManager : MonoBehaviour
+{
+    private Player player;
+    public static SaveLoadDataManager instance;
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = this; 
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        player = SetPlayer.player;
+    }
+
+    public void SaveGame()
+    {
+        ES2.Save(player.MaxHealth, "max health");
+        ES2.Save(PlayerLevel.Level, "player level");
+        ES2.Save(PlayerLevel.playerXP, "player xp");
+      
+    }
+}
